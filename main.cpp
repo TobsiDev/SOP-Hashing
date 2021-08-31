c065000d290a9b66e0cd678165421012dad877b9
Tobsi <tsh40884137@gmail.com>
2021-08-31 23:24:58 +0200

    Initial Hashing Test.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "testeronie.h"
+
+int main(int argc, char *argv[])
+{
+    ping("TestHash1234");
+    return 1;
+}
---
+++ src/testeronie.cpp
@@ -0,0 +1,7 @@
+#include "testeronie.h"
+
+int ping(const char *HashStr)
+{
+    std::cout << "Original Hash: " << HashStr << std::endl;
+    return 100;
+}
