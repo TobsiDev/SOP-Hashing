dd39339a3cd1566561bc2468bfcdcfd4aa4df7e0
Tobsi <tsh40884137@gmail.com>
2021-08-31 23:33:24 +0200

    Dev Branch Test.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+// Uses the Visual Studio Community 2019 Release - amd64
 #include "testeronie.h"
 
 int main(int argc, char *argv[])
 {
-    ping("TestHash1234");
+    ping("TestHash1234-321");
     return 1;
 }
